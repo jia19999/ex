f84df516364d756b3cbe1d1f381e0345efa466ca
jiaxinof1999 <jiaxinof1999@outlook.com>
2021-07-12 17:09:43 +0800

    second mod

--- fz.c
+++ fz.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
 void main()
 {
-	char s[]="this is a practise by jiaxin";
-		printf("%s",s);//before
-		fz(s);
-		printf("%s",s);//after
+	char s[20];
+	printf("limit 20 char\n");
+	scanf("%s",s)
+		printf("%s\n",s);//before
+	int len=strlen(s)-1;
+		fz(s,len);
+		printf("%s\n",s);//after
 }
-
+//second mofify 16:49
+void fz(char a[],int len){
+	int left=0;
+	int right=len;
+	while(left<=right){
+        char tmp =a[left];
+	a[left]=a[right];
+	a[right]=tmp;
+	left++;
+	right++;
+	}
+}
